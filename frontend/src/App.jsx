3002c8c05d326db31528f21b3f649f64d7688ba2
barouchmaxime <barouchmaxime@gmail.com>
2025-05-19 15:34:09 +0300

    remove dummy code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,6 @@ import RuleTable from "./components/RuleTable";
 function App() {
   return (
     <div className="min-h-screen bg-background text-foreground">
-      <button className="bg-red-500 text-white p-4">Test Button</button>
       <RuleTable />
     </div>
   );
